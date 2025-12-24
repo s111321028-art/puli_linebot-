3f17e438b3e6cc43ea17eb01c6987ab65d816f25
s111321028-art <s111321028@mail1.ncnu.edu.tw>
2025-12-25 00:04:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,9 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 if __name__ == "__main__":
+    # 從環境變數讀取 PORT，若無則預設 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-    app.run(host="0.0.0.0", port=10000)
 
 
